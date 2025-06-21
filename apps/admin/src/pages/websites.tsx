7ed75e1f0fe0306459d4916f7f0c3bb266aacf88
Ebalo <12586380+ebalo55@users.noreply.github.com>
2025-06-21 11:23:51 +0200

    feat: add class name utility function and hide leftover empty state

--- apps/admin/src/pages/websites.tsx
+++ apps/admin/src/pages/websites.tsx
@@ -16,13 +16,21 @@ import {
   IconTrash,
 } from "@tabler/icons-react";
 import { modals } from "@mantine/modals";
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { WEBSITE_QUERY_KEY } from "../lib/const";
-import { ADVANCED_QUERY_SCHEMA } from "@open-voices/validation/advanced-schemas";
+import {
+  ADVANCED_QUERY_SCHEMA,
+  FILTER_SCHEMA,
+} from "@open-voices/validation/advanced-schemas";
 import { z } from "zod/v4";
 import { parametrizeAdvancedQuery } from "../lib/parametrize-advanced-query";
+import { useForm } from "@mantine/form";
+import { zod4Resolver } from "mantine-form-zod-resolver";
+import { Filter } from "../components/filters";
+import { cn } from "../lib/cn";
 
 type Schema = z.infer<typeof ADVANCED_QUERY_SCHEMA>;
+type FilterType = NonNullable<Schema["filters"]>[number];
 
 async function getWebsites(
   page: number,
@@ -66,8 +74,26 @@ export const Websites: FC = () => {
     direction: "asc",
   });
 
+  const filters_form = useForm({
+    initialValues: {
+      id: undefined as FilterType | undefined,
+      name: undefined as FilterType | undefined,
+      url: undefined as FilterType | undefined,
+      description: undefined as FilterType | undefined,
+    },
+    validate: zod4Resolver(
+      z.object({
+        id: FILTER_SCHEMA.optional(),
+        name: FILTER_SCHEMA.optional(),
+        url: FILTER_SCHEMA.optional(),
+        description: FILTER_SCHEMA.optional(),
+      })
+    ),
+    mode: "controlled",
+  });
+
   const { data: websites, isLoading } = useQuery({
-    queryKey: [WEBSITE_QUERY_KEY, page, limit, sortStatus],
+    queryKey: [WEBSITE_QUERY_KEY, page, limit, sortStatus, filters_form.values],
     queryFn: () =>
       getWebsites(
         page,
@@ -76,8 +102,20 @@ export const Websites: FC = () => {
           by: sortStatus.columnAccessor,
           direction: sortStatus.direction,
         },
-        []
+        Object.entries(filters_form.values)
+          .map(([key, value]) => {
+            if (value === undefined) {
+              return false;
+            }
+
+            return {
+              ...value,
+              by: key,
+            } as FilterType;
+          })
+          .filter((v) => v !== false)
       ),
+    placeholderData: keepPreviousData,
   });
   const [records, setRecords] = useState<WebsitesType>([]);
 
@@ -121,27 +159,109 @@ export const Websites: FC = () => {
           columns={[
             {
               accessor: "id",
-              // this column has a custom title
               title: "#",
-              // right-align column
               textAlign: "right",
               width: 150,
               ellipsis: true,
+              filter({ close }) {
+                return (
+                  <Filter
+                    _type="string"
+                    field="id"
+                    onChange={(value) =>
+                      filters_form.setFieldValue("id", value)
+                    }
+                    value={filters_form.values.id}
+                    close={close}
+                    withClearButton
+                    withAdvancedControls
+                  />
+                );
+              },
+              filtering: !!filters_form.values.id,
+              filterPopoverProps: {
+                withArrow: true,
+                arrowRadius: 4,
+                arrowSize: 10,
+              },
             },
             {
               accessor: "name",
               width: 300,
               sortable: true,
               ellipsis: true,
+              filter({ close }) {
+                return (
+                  <Filter
+                    _type="string"
+                    field="name"
+                    onChange={(value) =>
+                      filters_form.setFieldValue("name", value)
+                    }
+                    value={filters_form.values.name}
+                    close={close}
+                    withClearButton
+                    withAdvancedControls
+                  />
+                );
+              },
+              filtering: !!filters_form.values.name,
+              filterPopoverProps: {
+                withArrow: true,
+                arrowRadius: 4,
+                arrowSize: 10,
+              },
             },
             {
               accessor: "url",
               width: 300,
               sortable: true,
               ellipsis: true,
+              filter({ close }) {
+                return (
+                  <Filter
+                    _type="string"
+                    field="url"
+                    onChange={(value) =>
+                      filters_form.setFieldValue("url", value)
+                    }
+                    value={filters_form.values.url}
+                    close={close}
+                    withClearButton
+                    withAdvancedControls
+                  />
+                );
+              },
+              filtering: !!filters_form.values.url,
+              filterPopoverProps: {
+                withArrow: true,
+                arrowRadius: 4,
+                arrowSize: 10,
+              },
             },
             {
               accessor: "description",
+              filter({ close }) {
+                return (
+                  <Filter
+                    _type="string"
+                    field="description"
+                    onChange={(value) =>
+                      filters_form.setFieldValue("description", value)
+                    }
+                    value={filters_form.values.description}
+                    close={close}
+                    withClearButton
+                    withAdvancedControls
+                  />
+                );
+              },
+              filtering: !!filters_form.values.description,
+              filterPopoverProps: {
+                withArrow: true,
+                arrowRadius: 4,
+                arrowSize: 10,
+              },
               render(record) {
                 return (
                   <Text c="dimmed" className={"whitespace-pre-line text-sm"}>
@@ -188,7 +308,14 @@ export const Websites: FC = () => {
             },
           ]}
           emptyState={
-            <div className="flex flex-col items-center justify-center gap-2 py-8 w-full">
+            <div
+              className={cn(
+                "flex flex-col items-center justify-center gap-2 py-8 w-full",
+                {
+                  hidden: records.length === 0,
+                }
+              )}
+            >
               <IconDatabaseExclamation
                 className="text-black mb-4"
                 size={40}
